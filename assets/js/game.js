eb8c5b6f45ea5080a60e45a40fa6077dc9c6a9a2
deb boro <debajit.boro666@gmail.com>
2022-05-11 00:02:37 -0400

    shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -85,6 +85,48 @@ var endGame = function () {
   }
 }
 
+var shop = function () {
+  //ask the player what they'd like to do
+  var shopOptionPrompt = window.prompt(
+    "Would you like to REFILL your health, UPGRADE your attack , or LEAVE the store? Please enter one : 'REFILL' , 'UPGRADE' or 'LEAVE' to make a choice",
+  )
+  // console.log('enter the shop')
+  switch (shopOptionPrompt) {
+    case 'refill':
+    case 'REFILL':
+      if (playerMoney >= 7) {
+        window.alert("Refilling player's health by 20 for 7 dollars")
+        //increase health and decrease money
+        playerHealth = playerHealth + 20
+        playerMoney = playerMoney - 7
+      } else {
+        window.alert("You don't have enough money")
+      }
+      break
+
+    case 'upgrade':
+    case 'UPGRADE':
+      if (playerMoney >= 7) {
+        window.alert("Upgrade player's attack by 6 for 7 dollars.")
+        playerAttack = playerAttack + 6
+        playerMoney = playerMoney - 7
+      } else {
+        window.alert("You don't have enough money")
+      }
+      break
+
+    case 'leave':
+    case 'LEAVE':
+      window.alert('leave the store')
+      break
+    default:
+      window.alert('You did not pick a valid option. Try again')
+
+      shop()
+      break
+  }
+}
+
 var startGame = function () {
   playerHealth = 100
   playerAttack = 10
@@ -96,6 +138,16 @@ var startGame = function () {
     if (playerHealth > 0) {
       window.alert('Welcome to Robot Gladiators ! Round' + (i + 1))
       fight(pickedEnemyName)
+      if (playerHealth > 0 && i < enemyNames.length - 1) {
+        //ask if the player want to use the stor before the next round
+        var storeConfirm = window.confirm(
+          'The fight is over, visit the store before the next round?',
+        )
+        // if yes, take them to the store() function
+        if (storeConfirm) {
+          shop()
+        }
+      }
     } else window.alert('You have lost your robot in battle! Game Over!')
   }
   endGame()
